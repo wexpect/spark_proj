47b151c4e39a20a58f9be0cc915195411ad1f373
Rui Wang <ray-wang@outlook.com>
2016-10-20 01:23:28 -0700

    add RDD operation

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -1,20 +1,46 @@
 /*
-http://spark.apache.org/docs/latest/quick-start.html#self-contained-applications
- */
+http://spark.apache.org/docs/latest/quick-start.html
+http://spark.apache.org/docs/latest/programming-guide.html
+*/
 
 import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "spark_note.txt"
-    val conf = new SparkConf().setAppName("Simple Application")
+    val conf = new SparkConf().setMaster("local[2]").setAppName("Simple Application")
     val sc = new SparkContext(conf)
-    val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
 
+    // Option1: create RDD from file
+    val logFile = "data/input/*.txt"
+    val logData = sc.textFile(logFile, 2).persist()
+    // logData.unpersist()  // manual remove an RDD
+
+    // collect entire RDD to driver program, may OOM
+    logData.collect().foreach(println)
+    // take first 10 to driver program
+    logData.take(10).foreach(println)
+
+    val wordCounts = logData.flatMap(line => line.split(" ")).
+      map(k => (k, 1)).reduceByKey((a,b) => a+b).sortByKey()
+    println("wordCounts")
+    wordCounts.take(10).foreach(println)
+    wordCounts.saveAsTextFile("data/output/word_counts")
+
+    val numAs = logData.filter(line => line.contains("a")).count()
+//    val numBs = logData.filter(line => line.contains("b")).count()
+    val numBs = logData.filter(MyFuncs.func1).count()
     println("\nLines with a: %s, Lines with b: %s \n".format(numAs, numBs))
+
+    // Option 2: create RDD from seq
+    val data = Array(1, 2, 3)
+    val distData = sc.parallelize(data)
+    distData.take(10).foreach(println)
+  }
+}
+
+object MyFuncs {
+  def func1(s: String): Boolean = {
+    return s.contains("b")
   }
 }
